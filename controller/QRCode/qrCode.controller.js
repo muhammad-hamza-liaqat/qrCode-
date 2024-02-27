4ad4525aa48de8f1f4b6ad73c5d3752ac113f5fe
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-02-27 10:08:37 +0500

    code clean

--- controller/QRCode/qrCode.controller.js
+++ controller/QRCode/qrCode.controller.js
@@ -13,15 +13,20 @@ const generateQRCode = async (req, res) => {
     const qrCode = await qr.toDataURL(qrCodeText);
     console.log("qrCode generated=>", qrCode);
 
-    // to database
+    // storing the record in the DB.
     const newBaseURL = await qrCodeModel.create({
-        image: qrCode
-    })
+      image: qrCode,
+    });
     console.log("base64 saved to database");
 
+    // response will send message, code base64URL, and id of the record in the database
     return res
       .status(200)
-      .json({ message: "qr generated successfully!", code: qrCode });
+      .json({
+        message: "qr generated successfully!",
+        _id: newBaseURL._id,
+        code: qrCode,
+      });
   } catch (error) {
     console.log("error occured at qr code controller ", error);
     return res
@@ -30,21 +35,25 @@ const generateQRCode = async (req, res) => {
   }
 };
 
+// the id and URL is stored in the DB.
 const getData = async (req, res) => {
-    const id = req.params.id;
-    if (!id) {
-        return res.status(400).json({ message: "ID field is required from params!" });
+  const id = req.params.id;
+  if (!id) {
+    return res
+      .status(400)
+      .json({ message: "ID field is required from params!" });
+  }
+  try {
+    const qrData = await qrCodeModel.findById(id);
+    if (!qrData) {
+      return res.status(404).json({ message: "QR code not found!" });
     }
-    try {
-        const qrData = await qrCodeModel.findById(id);
-        if (!qrData) {
-            return res.status(404).json({ message: "QR code not found!" });
-        }
-        res.send(`<img src="${qrData.image}" alt="QR Code converted">`);
-
-    } catch (error) {
-        console.log("Error occurred while fetching QR code from database:", error);
-        return res.status(500).json({ message: "Internal server error", error: error });
-    }
+    res.send(`<img src="${qrData.image}" alt="QR Code converted">`);
+  } catch (error) {
+    console.log("Error occurred while fetching QR code from database:", error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error });
+  }
 };
 module.exports = { generateQRCode, getData };
