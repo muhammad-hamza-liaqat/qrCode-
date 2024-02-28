792e71967b4d5ef5d1b08ce993e2d8a466b26bd3
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-02-28 14:59:00 +0500

    .

--- routes/d3Routes/d3.route.js
+++ routes/d3Routes/d3.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { makingTree } = require("../../controller/d3.js/d3.controller");
 const d3Routes = express.Router();
 
+
 d3Routes.route("/make-tree").post(makingTree);
 
 module.exports = { d3Routes };
